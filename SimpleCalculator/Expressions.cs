e70c2d63a9430b035fa09bf0f672386851249143
Bernard Anderson <bernardanderson@gmail.com>
2016-08-15 19:14:52 -0500

    Added basic command prompt and input vars

---
+++ SimpleCalculator/Expressions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Expressions
+    {
+        public int currentCommandCount { get; set; } = 0;
+        public bool userHasNotExited { get; set; } = true;
+        public string userInputFromCommandPrompt { get; set; }
+        public int firstNumericTerm { get; set; }
+        public int secondNumericTerm { get; set; }
+        public char operatorFromUserInput { get; set; }
+
+        public void ParseUserInput()
+        {
+            if (userInputFromCommandPrompt.ToLower() == "quit" | userInputFromCommandPrompt.ToLower() == "exit")
+            {
+                userHasNotExited = false;
+            }
+        }
+
+
+
+    }
+}
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,6 +10,18 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            // Creates a new instance of the Expressions class to give non-static access to the methods
+            Expressions newUserCalcInstance = new Expressions();
+
+
+            while (newUserCalcInstance.userHasNotExited)
+            {
+                Console.Write($"[{ newUserCalcInstance.currentCommandCount }] >");
+                newUserCalcInstance.userInputFromCommandPrompt = Console.ReadLine();
+
+                Console.Write(newUserCalcInstance.userInputFromCommandPrompt);
+
+            }
         }
     }
 }
