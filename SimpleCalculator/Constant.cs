de92f8e56a4726c8b4b1686f550863f63be38c48
Bernard Anderson <bernardanderson@gmail.com>
2016-08-18 13:27:26 -0500

    Constant class and methods made. The Constant class and two methods were made for setting and getting from the constant dictionary.

---
+++ SimpleCalculator/Constant.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Constant
+    {
+        // Sets the private constant dictionary
+        Dictionary<string, int> constantList;
+
+        // This checks to see if the constant already exists in the dictionary.  If it does, it returns "true" for success and
+        //  the value in a KeyValuePair.  If it doesn't exist it says "false" meaning that the dictionary pull failed.
+        public KeyValuePair<bool, int> ReturnConstantValue(string sentConstantLetter)
+        {
+            KeyValuePair<bool, int> isFoundInDictionaryValue;
+
+            if (constantList.ContainsKey(sentConstantLetter.ToLower()))
+            {
+                int tempConstantValue = constantList[sentConstantLetter.ToLower()];
+                isFoundInDictionaryValue = new KeyValuePair<bool, int> (true, tempConstantValue);
+            }
+            else
+            {
+                isFoundInDictionaryValue = new KeyValuePair<bool, int>(false, -1);
+            }
+            return isFoundInDictionaryValue;
+        }
+
+        // This sets the constant/value pair in the constantList dictionary.  If the dictionary already contains the letter,
+        //  then a "false" is returned for a fail. Otherwise a "true" is returned saying that the constant was successfully set.
+        public bool assignConstantValue(string sentConstantLetter, int sentConstantValue)
+        {
+            bool wasTheSettingSuccessful;
+
+            if (constantList.ContainsKey(sentConstantLetter.ToLower()))
+            {
+                wasTheSettingSuccessful = false;
+            }
+            else
+            {
+                constantList.Add(sentConstantLetter.ToLower(), sentConstantValue);
+                wasTheSettingSuccessful = true;
+            }
+
+            return wasTheSettingSuccessful;
+        }
+
+
+    }
+}
